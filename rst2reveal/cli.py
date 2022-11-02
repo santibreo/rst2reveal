d15f823e8ecee193a996ec55ac1dea3d648413a4
Santiago Breogán Pérez Pita <santiago.breogan@quant.global>
2022-11-02 08:31:23 +0100

    Use existing config when input_file is .rst file

--- rst2reveal/cli.py
+++ rst2reveal/cli.py
@@ -2,10 +2,11 @@
 from __future__ import annotations
 import sys
 from argparse import ArgumentParser
+import configparser as ConfigParser
 from pathlib import Path
 from .Parser import Parser
 from . import PYGMENTS_STYLES, REVEAL_THEMES, REVEAL_TRANSITIONS
-from typing import Optional, Sequence
+from typing import Optional, Sequence, Union
 
 def is_config_file(filepath: Path) -> bool:
     """
@@ -13,16 +14,30 @@ def is_config_file(filepath: Path) -> bool:
     """
     return filepath.suffix in {'.cfg', '.conf', '.ini' }
 
+
+def has_config_file(filepath: Path) -> Union[Path, None]:
+    """
+    Check if configuration file exists for passed file
+    """
+    if (
+        (conf_filepath := filepath.with_suffix('.cfg')).exists()
+        or (conf_filepath := filepath.with_suffix('.conf')).exists()
+        or (conf_filepath := filepath.with_suffix('.ini')).exists()
+    ):
+        return conf_filepath
+    return None
+
+
 # Allowed themes and transitions
 def main(argv: Optional[Sequence[str]] = None) -> int:
     # Define arguments {{{
     parser = ArgumentParser()
-    parser_boolean_arguments = {'slidenos', 'no_controls', 'progress'}
+    parser_boolean_arguments = {'slidenos', 'no_controls', 'no_progress'}
     parser.description="""rst2reveal: ReST to Reveal.js slide generator."""
     # Name of the ReST file to process
     parser.add_argument(
         "input_file", type=str,
-        help="Path to ReStructuredText file to parse (.rst or .txt) or a configuration file defining all the options (.ini, .conf, .cfg)."
+        help="ReStructuredText file or configuration file to be parsed."
     )
     # Theme to use
     parser.add_argument("-t", "--theme",
@@ -57,12 +72,11 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
     if input_file.exists() and not input_file.is_file():
         print(f'ERROR: {input_file!s} is not a valid file', file=sys.stderr)
         return 1
-    # Create configuration file {{{
-    if not is_config_file(input_file) or not input_file.exists():
+    # Create configuration file if needed {{{
+    if not is_config_file(input_file) and has_config_file(input_file) is None:
         # Copy default config file to given location
-        input_file = input_file.with_suffix('conf')
+        input_file = input_file.with_suffix('.conf')
         print(f'Creating the configuration file {input_file!s}.')
-        import configparser as ConfigParser
         config = ConfigParser.RawConfigParser()
         config.add_section('rst2reveal')
         config.set('rst2reveal', 'input_file', str(input_file.with_suffix('.rst')))
@@ -88,11 +102,12 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
             '%(author)s%(is_institution)s %(institution)s. %(date)s'
         ))
         # Writing our configuration file
-        config.write(input_file.open('w'))
+        config.write(input_file.open('w', encoding='latin-1'))
+    elif not is_config_file(input_file):
+        input_file = has_config_file(input_file)
     #  }}}
     # Read configuration file {{{
     print(f'Reading from the configuration file {input_file!s}.')
-    import configparser as ConfigParser
     config_parser = ConfigParser.RawConfigParser()
     config_parser.read(input_file)
     config_args = config_parser["rst2reveal"]
